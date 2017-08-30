d1cef4c6c56c0b48ca3a20af629b859aac48e548
k.orlov <k.orlov@maximaster.ru>
2017-08-30 17:07:02 +0300

    #2 added menu in header

--- .top.menu.php
+++ .top.menu.php
@@ -1,46 +1,11 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 $aMenuLinks = Array(
 	Array(
-		"Как купить", 
-		"about/howto/", 
-		Array(), 
-		Array(), 
-		"" 
-	),
-	Array(
-		"Доставка", 
-		"about/delivery/", 
-		Array(), 
-		Array(), 
-		"" 
-	),
-	Array(
-		"О магазине", 
-		"about/", 
-		Array(), 
-		Array(), 
-		"" 
-	),	
-	Array(
-		"Гарантия", 
-		"about/guaranty/", 
-		Array(), 
-		Array(), 
-		"" 
-	),
-	Array(
-		"Контакты",
-		"about/contacts/",
+		"home",
+		"/",
 		Array(),
 		Array(),
 		""
-	),
-	Array(
-		"Мой кабинет",
-		"personal/",
-		Array(),
-		Array(),
-		"CUser::IsAuthorized()"
 	),
 );
 ?>
--- local/templates/explore/footer.php
+++ local/templates/explore/footer.php
@@ -1,4 +1,6 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+            </div>
+
 			<footer class="footer">
 				<div class="container">
 					<div class="row justify-content-between border-bottom">
@@ -105,7 +107,5 @@
 					</div>
 				</div>
 			</footer>
-            <?php $APPLICATION->ShowHeadStrings();?>
-            <?php $APPLICATION->ShowHeadScripts();?>
 	</body>
 	</html>
